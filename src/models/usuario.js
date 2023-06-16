2530e23ff424976863f8fdbf6ead66c319237736
gabrielrramalho <biel.rramalho@gmail.com>
2023-06-16 02:06:28 -0300

    alteração usuario.js

---
+++ src/models/usuario.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+const Pessoa = require('./pessoa');
+
+
+const Usuario = database.define('usuario', {
+    usuario_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+
+    senha: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    pessoaId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true,
+        references: {
+            model: Pessoa,
+            key: 'id',
+        }
+    }
+
+})
+
+Pessoa.hasOne(Usuario, { foreignKey: 'pessoaId' });
+Usuario.belongsTo(Pessoa, { foreignKey: 'pessoaId' });
